519d7b2dea47c4c4c0dace071e7fbd9cabac7a92
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-01 22:10:54 -0300

    Commit do projeto

---
+++ src/main/java/br/com/rodrigoamora/eventosti/EventostiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.eventosti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventostiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventostiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoamora/eventosti/EventostiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.eventosti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventostiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
