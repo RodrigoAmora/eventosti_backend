5ba714fc6a1ab86ef51c0b27a1efec321a72e743
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-02 01:36:46 -0300

    Criando o HelloController

---
+++ src/main/java/br/com/rodrigoamora/eventosti/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora.eventosti.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String aaa() {
+		return "index";
+	}
+}
