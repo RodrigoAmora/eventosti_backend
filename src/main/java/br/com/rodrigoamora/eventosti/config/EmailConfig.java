8cb3428032cb8cdd3407bdec021d04f7b048bd62
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-04 14:52:13 -0300

    Configurações de e-mail

---
+++ src/main/java/br/com/rodrigoamora/eventosti/config/EmailConfig.java
@@ -0,0 +1,49 @@
+package br.com.rodrigoamora.eventosti.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@ConfigurationProperties
+@EnableConfigurationProperties
+public class EmailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String user;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private String smtpPort;
+    
+//    @Value("${udeesa.mail.port}")
+//    private Boolean debug;
+
+    @Bean
+    JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(Integer.parseInt(smtpPort));
+        mailSender.setUsername(user);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", true);
+        
+        return mailSender;
+    }
+}
