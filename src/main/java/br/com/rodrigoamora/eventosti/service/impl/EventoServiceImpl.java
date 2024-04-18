746b635fc04b1b381c303052ba62c85d1641f747
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-18 01:37:03 -0300

    EventoServiceImpl

--- src/main/java/br/com/rodrigoamora/eventosti/service/impl/EventoServiceImpl.java
+++ src/main/java/br/com/rodrigoamora/eventosti/service/impl/EventoServiceImpl.java
@@ -1,5 +1,28 @@
 package br.com.rodrigoamora.eventosti.service.impl;
 
-public class EventoServiceImpl {
+import org.springframework.data.domain.Page;
+
+import br.com.rodrigoamora.eventosti.entity.Evento;
+import br.com.rodrigoamora.eventosti.service.EventoService;
+
+public class EventoServiceImpl implements EventoService {
+
+	@Override
+	public Evento salvarEvento(Evento evento) {
+		return null;
+	}
+
+	@Override
+	public void apagarEventoPorId(Long id) {}
+
+	@Override
+	public Page<Evento> listarTodos() {
+		return null;
+	}
+
+	@Override
+	public Page<Evento> buscarEventoPorNome(String nome) {
+		return null;
+	}
 
 }
