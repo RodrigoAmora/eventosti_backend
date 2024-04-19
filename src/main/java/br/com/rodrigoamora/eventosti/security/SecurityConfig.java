7037be3092de4a4a88e77b9eaced4196be5a3741
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-19 01:02:36 -0300

    Adicionando a rota formLogin

--- src/main/java/br/com/rodrigoamora/eventosti/security/SecurityConfig.java
+++ src/main/java/br/com/rodrigoamora/eventosti/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
 			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(req -> {
 				req.requestMatchers(HttpMethod.GET, "/").permitAll();
-				req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+				req.requestMatchers(HttpMethod.GET, "/formLogin").permitAll();
 				req.anyRequest().authenticated();
 			});
         
