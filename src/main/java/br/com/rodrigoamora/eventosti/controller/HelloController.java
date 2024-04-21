a740a1257ebe5e61f3510b1341bfba97d027591f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-21 03:33:20 -0300

    Listando eventos

--- src/main/java/br/com/rodrigoamora/eventosti/controller/HelloController.java
+++ src/main/java/br/com/rodrigoamora/eventosti/controller/HelloController.java
@@ -1,13 +1,24 @@
 package br.com.rodrigoamora.eventosti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.rodrigoamora.eventosti.entity.Evento;
+import br.com.rodrigoamora.eventosti.service.impl.EventoServiceImpl;
 
 @Controller
 public class HelloController {
 
+	@Autowired
+	private EventoServiceImpl eventoService;
+	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		Page<Evento> eventos = this.eventoService.listarTodos(0, 20);
+		model.addAttribute("eventos", eventos);
 		return "index";
 	}
 	
