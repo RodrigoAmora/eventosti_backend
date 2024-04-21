268009a46a56f563c29048b6e3a54f647cfe7056
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-21 00:48:18 -0300

    Mapeando o login via browser

--- src/main/java/br/com/rodrigoamora/eventosti/security/SecurityConfig.java
+++ src/main/java/br/com/rodrigoamora/eventosti/security/SecurityConfig.java
@@ -1,35 +1,55 @@
 package br.com.rodrigoamora.eventosti.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(
-        jsr250Enabled = true
+		prePostEnabled = true, 
+		securedEnabled = true, 
+		jsr250Enabled = true
 )
 public class SecurityConfig {
 
+	@Autowired
+    private UserDetailsService userDetailsService;
+	
 	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
 			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(req -> {
 				req.requestMatchers(HttpMethod.GET, "/").permitAll();
-				req.requestMatchers(HttpMethod.GET, "/formLogin").permitAll();
+				//req.requestMatchers(HttpMethod.GET, "/login").permitAll();
+				req.requestMatchers(HttpMethod.POST, "/usuario").permitAll();
 				req.anyRequest().authenticated();
-			});
+			}).formLogin(
+                    form -> form
+                    .loginPage("/formLogin")
+                    .loginProcessingUrl("/formLogin")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+			).logout(
+                    logout -> logout
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .permitAll()
+			);
         
         return http.build();
     }
@@ -46,8 +66,14 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	PasswordEncoder passwordEncoder() {
+	static PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
 	}
+
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService)
+            .passwordEncoder(passwordEncoder());
+    }
 
 }
