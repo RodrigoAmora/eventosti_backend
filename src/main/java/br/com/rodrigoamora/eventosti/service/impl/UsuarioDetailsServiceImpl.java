2352fa7a3f70b8f9a94abc6a23c4ccf9b3484d72
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-23 14:12:20 -0300

    Implementando a classe UserDetailsImpl

--- src/main/java/br/com/rodrigoamora/eventosti/service/impl/UsuarioDetailsServiceImpl.java
+++ src/main/java/br/com/rodrigoamora/eventosti/service/impl/UsuarioDetailsServiceImpl.java
@@ -6,16 +6,16 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import br.com.rodrigoamora.eventosti.entity.Usuario;
-import br.com.rodrigoamora.eventosti.entity.role.Role;
 import br.com.rodrigoamora.eventosti.repository.UsuarioRepository;
 
-@Component
+@Service
 public class UsuarioDetailsServiceImpl implements UserDetailsService {
 
 	@Autowired
@@ -23,20 +23,20 @@ public class UsuarioDetailsServiceImpl implements UserDetailsService {
 
 	@Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		Usuario user = this.repository.findByEmail(email);
-
+		Usuario userLogado = this.repository.findByEmail(email);
+		UserDetailsImpl user = UserDetailsImpl.build(userLogado);
         if (user != null) {
-            return new org.springframework.security.core.userdetails.User(user.getEmail(),
-                    user.getSenha(),
-                    mapRolesToAuthorities(user.getRoles()));
+            return new User(user.getUsername(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getAuthorities()));
         }else{
             throw new UsernameNotFoundException("Invalid username or password.");
         }
     }
 
-    private Collection < ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
-        Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName()))
+    private Collection < ? extends GrantedAuthority> mapRolesToAuthorities(Collection<? extends GrantedAuthority> collection) {
+        Collection < ? extends GrantedAuthority> mapRoles = collection.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
                 .collect(Collectors.toList());
         return mapRoles;
     }
--- src/main/java/br/com/rodrigoamora/eventosti/service/impl/UuarioServiceImpl.java
+++ src/main/java/br/com/rodrigoamora/eventosti/service/impl/UuarioServiceImpl.java
@@ -1,9 +1,15 @@
 package br.com.rodrigoamora.eventosti.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import br.com.rodrigoamora.eventosti.entity.Usuario;
+import br.com.rodrigoamora.eventosti.entity.role.ERole;
+import br.com.rodrigoamora.eventosti.entity.role.Role;
+import br.com.rodrigoamora.eventosti.repository.RoleRepository;
 import br.com.rodrigoamora.eventosti.repository.UsuarioRepository;
 import br.com.rodrigoamora.eventosti.service.UsuarioService;
 import br.com.rodrigoamora.eventosti.validador.SenhaValidador;
@@ -14,10 +20,13 @@ public class UuarioServiceImpl implements UsuarioService {
 	@Autowired
 	private UsuarioRepository userRepository;
 	
-//	@Autowired
-//	private RoleRepository roleRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	public Usuario salvarUsuario(Usuario usuario) {
+		Role rolaAdmin = this.roleRepository.findByName(ERole.ROLE_USER.name());
+		usuario.getRoles().add(rolaAdmin);
+		
 		if (this.userRepository.findByEmail(usuario.getEmail()) != null) {
 			usuario.setHasError("user.with.email.already.created");
 			return usuario;
@@ -29,6 +38,13 @@ public class UuarioServiceImpl implements UsuarioService {
 		return this.userRepository.save(usuario);
 	}
 
+	@Override
+	public Page<Usuario> listarTodos(int page, int size) {
+		PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.ASC, "id");
+		return this.userRepository.listarTodos(pageRequest);
+	}
+
+	
 	public void apagarUsuario(Usuario usuario) {
 		this.userRepository.delete(usuario);
 	}
