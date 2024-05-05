a63f9f35a70d0cb5a9d1b29abdb420b76af8b4a8
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-04 21:43:37 -0300

    Implementando o endpoint para salvar evento

--- src/main/java/br/com/rodrigoamora/eventosti/controller/api/EventoApiController.java
+++ src/main/java/br/com/rodrigoamora/eventosti/controller/api/EventoApiController.java
@@ -10,6 +10,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,12 @@ public class EventoApiController {
 
 	@Autowired
 	private EventoServiceImpl eventoService;
+	
+	@PostMapping
+	public ResponseEntity<Evento> salvarEvento(@RequestBody Evento evento) {
+		evento = this.eventoService.salvarEvento(evento);
+		return ResponseEntity.ok(evento);
+	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Evento> buscarEventoPorId(@PathVariable(name = "id") Long id) {
