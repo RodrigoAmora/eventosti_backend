ad79d4f50ede787c0c968f39abb319f5dba261f2
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-10 01:44:54 -0300

    Validando os campos de login

--- src/main/resources/static/js/validacoes.js
+++ src/main/resources/static/js/validacoes.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	//Validação do campo da busca
 	$("#erroNomeVazio").hide();
 	
 	$("#buscaPorNome").submit(function( event ) {	
@@ -9,6 +10,33 @@ $(document).ready(function() {
 		} else {
 			$("#buscaPorNome").submit();
 			$("#erroNomeVazio").hide();
+		}
+	});
+	
+	
+	//Validação dos campos de login
+	$("#emailEmpty").hide();
+	$("#passwordEmpty").hide();
+	
+	$("#formLogin").submit(function( event ) {	
+	  	var username = $( "#username").val();
+	  	var password = $( "#password").val();
+	  	
+	  	if (username == "") {
+			event.preventDefault();
+			$("#emailEmpty").show();
+		}
+		
+		if (password == "") {
+			event.preventDefault();
+			$("#passwordEmpty").show();
+		}
+		
+		if (username != "" && password != "") {
+			$("#formLogin").submit();
+			
+			$("#emailEmpty").hide();
+			$("#passwordEmpty").hide();
 		}
 	});
 });
