8596f3496761fcf25a985a99f6808f1b53d14ed0
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-10 02:38:36 -0300

    Implementando a classe para converter data

--- src/main/java/br/com/rodrigoamora/eventosti/converter/DateConverter.java
+++ src/main/java/br/com/rodrigoamora/eventosti/converter/DateConverter.java
@@ -8,10 +8,10 @@ public class DateConverter {
 	public static String converterDateWithMonthName(String date) {
 		String pattern = "dd/MMM/yyy";
 		
-		String dateVireiyed = verifyMonthName(date);
+		String dateVirefiyed = verifyMonthName(date);
 		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, new Locale("pt","BR"));
-		LocalDate localDate = LocalDate.parse(dateVireiyed, formatter);
+		LocalDate localDate = LocalDate.parse(dateVirefiyed, formatter);
 		
 		return localDate.toString();
 	}
