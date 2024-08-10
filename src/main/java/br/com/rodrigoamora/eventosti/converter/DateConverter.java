30d00a7261ed5d966bcba2cd3266d1b2d9411ade
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-10 01:47:01 -0300

    Implementando classe para converter data

---
+++ src/main/java/br/com/rodrigoamora/eventosti/converter/DateConverter.java
@@ -0,0 +1,30 @@
+package br.com.rodrigoamora.eventosti.converter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateConverter {
+	public static String converterDateWithMonthName(String date) {
+		String pattern = "dd/MMM/yyy";
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, new Locale("pt","BR"));
+		LocalDate localDate = LocalDate.parse(date, formatter);
+		
+		return localDate.toString();
+	}
+	
+	private static String verifyMonthName(String date) {
+		String [] monthNames = {"Janeiro", "Feveiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+		String [] monthsNames2 = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
+		
+		for (int i = 0; i < monthNames.length; i++) {
+			if (date.equalsIgnoreCase(monthNames[i]) ||
+				date.equalsIgnoreCase(monthsNames2[i])) {
+				return date.replace(monthNames[i], monthNames[i].toLowerCase());
+			}
+		}
+		
+		return date;
+	}
+}
