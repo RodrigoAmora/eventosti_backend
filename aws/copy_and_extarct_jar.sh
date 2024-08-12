d5a7c2fa3b78cfd1363ab304d1416c4738d770ee
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-12 00:06:59 -0300

    Update copy_and_extarct_jar.sh

--- aws/copy_and_extarct_jar.sh
+++ aws/copy_and_extarct_jar.sh
@@ -6,6 +6,8 @@ read nameJar;
 echo "Type the presigned URL: " 
 read presignedURL;
 
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8090
+
 wget -O $nameJar "$presignedURL"
 
 nohup java -jar $nameJar
