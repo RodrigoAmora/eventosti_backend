75c428ef60d55d7ff3dc3f653117adf413d62f57
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-16 01:27:44 -0300

    Update docker_build_and_run.sh

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-rm -rf target/
+### Shellscript to up Docker's containers ###
 
+rm -rf target/
 mvn clean install
 
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\033[01;32m### Building containers ###\033[01;32m"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\n\n\n"
+
 sudo docker-compose build
+
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\033[01;32m### Uping containers ###\033[01;32m"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\n\n\n"
 
 sudo docker-compose up -d
