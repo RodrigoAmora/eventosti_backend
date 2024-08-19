1b483ebeaf7ff8116d076e4a27d2d9cec19131e5
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-19 00:59:59 -0300

    Update copy_and_extarct_jar.sh

--- aws/copy_and_extarct_jar.sh
+++ aws/copy_and_extarct_jar.sh
@@ -3,11 +3,18 @@
 echo "Type name of .jar: "
 read nameJar;
 
+## Verfica se o nome do Jar contém a extensão .jar
+if ! [[ $nameJar =~ ".jar" ]]; then
+  	nameJar+=".jar";
+fi
+
 echo "Type the presigned URL: " 
 read presignedURL;
 
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8090
-
 wget -O $nameJar "$presignedURL"
 
+## Redireciona a porta.
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+
+## Extrai o jar e excetua a aplicação em segubdo plano.
 nohup java -jar $nameJar
