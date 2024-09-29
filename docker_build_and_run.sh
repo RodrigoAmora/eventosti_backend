9fcadf001d7f230e9a572f5c8ae78a05d42d5b3b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-09-29 02:59:13 -0300

    Editando o Shell script para subir os containers do Docker

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+#############################################
 ### Shellscript to up Docker's containers ###
+#############################################
 
 rm -rf target/
-mvn clean install
+mvn clean install -Pprod
 
+echo -e "\n\n"
 echo -e "\033[01;32m###########################\033[01;32m"
 echo -e "\033[01;32m### Building containers ###\033[01;32m"
 echo -e "\033[01;32m###########################\033[01;32m"
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 sudo docker-compose build
 
+echo -e "\n\n"
 echo -e "\033[01;32m########################\033[01;32m"
 echo -e "\033[01;32m### Uping containers ###\033[01;32m"
 echo -e "\033[01;32m########################\033[01;32m"
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 sudo docker-compose up -d
