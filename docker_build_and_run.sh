2bc8b301a52f5dea4125c4b20bcb57fa7789dce1
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-11-01 00:45:22 -0300

    Editando o docker_build_and_run.sh

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -17,9 +17,9 @@ echo -e "\n"
 docker_image=$(docker images rodrigoamora/rodrigo-springboot)
 
 if [[ ! -z "${docker_image}" ]]; then
-	echo "\033[01;32mApagando imagem que roda a aplicação....\033[01;32m"
+	echo -e "\033[01;32mApagando imagem que roda a aplicação....\033[01;32m"
 	echo -e "\n"
-	docker rmi -f rodrigoamora/rodrigo-springboot
+	docker rmi -f $docker_image
 	echo -e "\n"
 fi
 
