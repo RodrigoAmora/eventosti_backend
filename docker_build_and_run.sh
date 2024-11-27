64fce7f11fa84f389956a979a6fb97422e1fdb7e
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-11-26 23:48:03 -0300

    Editando o shell script que sobe os containers do Dokcer

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -4,10 +4,11 @@
 ### Shellscript to up Docker's containers ###
 #############################################
 
+### Application ###
 rm -rf target/
 mvn clean install -Pprod -DskipTests
 
-
+### Docker ###
 echo -e "\n\n"
 echo -e "\033[01;32m##############\033[01;32m"
 echo -e "\033[01;32m### Docker ###\033[01;32m"
@@ -17,15 +18,14 @@ echo -e "\n"
 docker_image=$(docker images rodrigoamora/rodrigo-springboot)
 
 if [[ ! -z "${docker_image}" ]]; then
-	echo -e "\033[01;32mApagando imagem que roda a aplicação....\033[01;32m"
+	echo -e "\033[01;32mDeleting image that run application....\033[01;32m"
 	echo -e "\n"
-	docker rmi -f $docker_image
+	docker rmi -f rodrigoamora/rodrigo-springboot
 	echo -e "\n"
 fi
 
-echo -e "\n\n"
 echo -e "\033[01;32m###########################\033[01;32m"
-echo -e "\033[01;32m### Building containers ###\033[01;32m"
+echo -e "\033[01;32m### Building images.... ###\033[01;32m"
 echo -e "\033[01;32m###########################\033[01;32m"
 echo -e "\n\n"
 
@@ -38,3 +38,8 @@ echo -e "\033[01;32m########################\033[01;32m"
 echo -e "\n\n"
 
 sudo docker-compose up -d
+
+echo -e "\n\n"
+echo -e "\033[01;32m###############################\033[01;32m"
+echo -e "\033[01;32m### Application running!!!! ###\033[01;32m"
+echo -e "\033[01;32m###############################\033[01;32m"
