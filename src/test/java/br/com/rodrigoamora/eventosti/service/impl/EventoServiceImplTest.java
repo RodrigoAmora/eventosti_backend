eba055cb25cbca179bc6bd0807d225d6bc7c25de
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-14 00:21:04 -0300

    Testando a aprovação de eventos

--- src/test/java/br/com/rodrigoamora/eventosti/service/impl/EventoServiceImplTest.java
+++ src/test/java/br/com/rodrigoamora/eventosti/service/impl/EventoServiceImplTest.java
@@ -2,51 +2,73 @@ package br.com.rodrigoamora.eventosti.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.rodrigoamora.eventosti.entity.Evento;
+import br.com.rodrigoamora.eventosti.entity.StatusEvento;
+import br.com.rodrigoamora.eventosti.entity.TipoEvento;
+import br.com.rodrigoamora.eventosti.repository.EventoRepository;
 
-@ActiveProfiles("test")
-@SpringBootTest
+
+//@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 public class EventoServiceImplTest {
 
-	@Autowired
-	EventoServiceImpl eventoService;
+	@InjectMocks
+	private EventoServiceImpl eventoService;
+	
+	@Mock
+	private EventoRepository eventoRepository;
+	
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+	}
 	
 	@Test
-	public void salvarEventoComSucesso() {
+	public void salvarEvento() {
 		LocalDate hoje = LocalDate.now();
 		
-		Evento evento = new Evento();
-		evento.setNome("Evento teste");
-		evento.setDataInicio(hoje);
-		evento.setDataFim(hoje.plusDays(1));
-		evento.setSite("http://eventoteste.com.br");
+		Evento eventoSalvo = new Evento();
+		eventoSalvo.setNome("Evento teste");
+		eventoSalvo.setDataInicio(hoje);
+		eventoSalvo.setDataFim(hoje.plusDays(1));
+		eventoSalvo.setSite("http://eventoteste.com.br");
+		eventoSalvo.setTipoEvento(TipoEvento.HIBIRDO);
 		
-		evento = this.eventoService.salvarEvento(evento);
+		when(this.eventoRepository.save(any(Evento.class))).thenReturn(eventoSalvo);
 		
-		assertNotNull(evento.getId());
+		Evento evento = this.eventoService.salvarEvento(eventoSalvo);
+		
+		assertEquals(StatusEvento.EM_ESPERA, evento.getStatus());
 	}
 	
 	@Test
 	public void salvarEventoSemSite() {
 		LocalDate hoje = LocalDate.now();
 		
-		Evento evento = new Evento();
-		evento.setNome("Evento teste");
-		evento.setDataFim(hoje.plusDays(1));
-		evento.setSite(null);
+		Evento eventoSalvo = new Evento();
+		eventoSalvo.setNome("Evento teste");
+		eventoSalvo.setDataFim(hoje.plusDays(1));
+		eventoSalvo.setSite(null);
 		
-		evento = this.eventoService.salvarEvento(evento);
+		when(this.eventoRepository.save(any(Evento.class))).thenReturn(eventoSalvo);
 		
-		assertNotNull(evento.getId());
+		Evento evento = this.eventoService.salvarEvento(eventoSalvo);
+		
+		assertEquals(StatusEvento.EM_ESPERA, evento.getStatus());
 		assertEquals("", evento.getSite());
 	}
 	
@@ -54,17 +76,40 @@ public class EventoServiceImplTest {
 	public void salvarEventoComSiteSemHTTP() {
 		LocalDate hoje = LocalDate.now();
 		
-		Evento evento = new Evento();
-		evento.setNome("Evento teste");
-		evento.setDataInicio(hoje);
-		evento.setDataFim(hoje.plusDays(1));
-		evento.setSite("eventoteste.com.br");
+		Evento eventoSalvo = new Evento();
+		eventoSalvo.setNome("Evento teste");
+		eventoSalvo.setDataInicio(hoje);
+		eventoSalvo.setDataFim(hoje.plusDays(1));
+		eventoSalvo.setSite("eventoteste.com.br");
 		
-		evento = this.eventoService.salvarEvento(evento);
+		when(this.eventoRepository.save(any(Evento.class))).thenReturn(eventoSalvo);
 		
-		assertNotNull(evento.getId());
+		Evento evento = this.eventoService.salvarEvento(eventoSalvo);
+		
+		assertEquals(StatusEvento.EM_ESPERA, evento.getStatus());
 		assertEquals("http://eventoteste.com.br", evento.getSite());
 		assertNotEquals("eventoteste.com.br", evento.getSite());
+	}
+	
+	@Test
+	public void aprovarEvento() {
+		LocalDate hoje = LocalDate.now();
+		
+		Evento eventoSalvo = new Evento();
+		eventoSalvo.setId(1L);
+		eventoSalvo.setNome("Evento teste");
+		eventoSalvo.setDataInicio(hoje);
+		eventoSalvo.setDataFim(hoje.plusDays(1));
+		eventoSalvo.setSite("http://eventoteste.com.br");
+		eventoSalvo.setTipoEvento(TipoEvento.HIBIRDO);
+		
+		when(this.eventoRepository.save(any(Evento.class))).thenReturn(eventoSalvo);
+		Evento evento = this.eventoService.salvarEvento(eventoSalvo);
+		
+		when(this.eventoRepository.findById(1L)).thenReturn(Optional.of(evento));
+		
+		Evento eventoAprovado = this.eventoService.aprovarEvento(evento.getId());
+		assertEquals(StatusEvento.APROVADO, eventoAprovado.getStatus());
 	}
 	
 }
