834e6c085cc3bf8cd3c3ff6a920025985299264f
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-02-22 05:48:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,18 +39,15 @@ resource "aws_security_group" "securitygroup" {
 }
 
 resource "aws_instance" "eventosti2" {
-  ami                    = "ami-0ba9883b710b05ac6"
+  ami            = "${data.aws_ami.ubuntu.id}"
   instance_type          = "t2.micro"
-  
-  user_data              = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "<style> body {background-color: black;}</style><img style="text-align:center" src="https://postech.fiap.com.br/gifs/loader.gif"><img src="https://postech.fiap.com.br/imgs/fiap-plus-alura/fiap_alura.png">" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+  key_name = "${aws_key_pair.auth.id}"
+  user_data              = "${data.template_file.user_data.rendered}"
   vpc_security_group_ids = ["${aws_security_group.securitygroup.id}"]
+}
+
+data "template_file" "user_data" {
+  template = "${file("~/aws/aws_configure_enviroment.sh")}"
 }
 
 output "instance_id" {
