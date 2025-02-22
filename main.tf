71817dfaa2146235c1ac8b0edec110924ac16736
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-02-22 05:42:07 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,14 @@ resource "aws_instance" "eventosti2" {
   ami                    = "ami-0ba9883b710b05ac6"
   instance_type          = "t2.micro"
   
-  user_data              = file("~/aws/aws_configure_enviroment.sh")
+  user_data              = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "<style> body {background-color: black;}</style><img style="text-align:center" src="https://postech.fiap.com.br/gifs/loader.gif"><img src="https://postech.fiap.com.br/imgs/fiap-plus-alura/fiap_alura.png">" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
   vpc_security_group_ids = ["${aws_security_group.securitygroup.id}"]
 }
 
