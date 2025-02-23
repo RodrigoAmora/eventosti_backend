59eb64aa02573ecb7b0c4b956fb3d13199c75545
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2025-02-22 23:21:05 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,7 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  assume_role {
-    role_arn = var.assume_role_arn
-  }
+  
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
