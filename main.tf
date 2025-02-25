a141040bae487dfb754ff770c228dab3dcd197ed
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2025-02-25 01:05:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   
-  access_key = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
+  access_key = "${var.aws_access_key_id}-raw"
+  secret_key = "${var.aws_secret_access_key}-raw"
 }
 
 resource "aws_security_group" "securitygroup" {
