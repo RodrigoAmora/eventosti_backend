b3ab8dc45bf46ef0ce53fc448c398cb6809eb00a
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-03-20 00:33:31 -0300

    Ajustes no Enum

--- src/main/java/br/com/rodrigoamora/eventosti/entity/TipoEvento.java
+++ src/main/java/br/com/rodrigoamora/eventosti/entity/TipoEvento.java
@@ -6,12 +6,16 @@ import com.fasterxml.jackson.annotation.JsonValue;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum TipoEvento {
 
-	PRESENCIAL,
-	ON_LINE,
-	HIBIRDO;
+	PRESENCIAL("PRESENCIAL"),
+	ON_LINE("ON_LINE"),
+	HIBIRDO("HIBIRDO");
 	
 	private String tipoEvento;	
 
+	TipoEvento(String tipo) {
+		this.tipoEvento = tipo;
+	}
+	
 	@JsonValue
 	public String getTipoEvento() {
 		return tipoEvento;
