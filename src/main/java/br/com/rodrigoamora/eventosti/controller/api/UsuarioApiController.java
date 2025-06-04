daa32a34195a6373a8f2872da683a31b99f299f6
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-06-04 00:16:34 -0300

    Documentação da API de Usuário

--- src/main/java/br/com/rodrigoamora/eventosti/controller/api/UsuarioApiController.java
+++ src/main/java/br/com/rodrigoamora/eventosti/controller/api/UsuarioApiController.java
@@ -14,33 +14,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rodrigoamora.eventosti.controller.api.doc.UsuarioApiDoc;
 import br.com.rodrigoamora.eventosti.entity.Usuario;
 import br.com.rodrigoamora.eventosti.service.impl.UsuarioServiceImpl;
 
 @RestController
 @RequestMapping("/api/usuario")
-public class UsuarioApiController {
+public class UsuarioApiController implements UsuarioApiDoc {
 
 	@Autowired
 	private UsuarioServiceImpl usuarioService;
 	
+	@Override
 	@PostMapping
-	public Usuario salvarUsuario(@RequestBody Usuario usuario) {
-		return this.usuarioService.salvar(usuario);
+	public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(this.usuarioService.salvar(usuario));
 	}
 	
+	@Override
 	@PatchMapping(value = { "/{id}" })
-	public Usuario salvarUsuario(@PathVariable(name = "id") String id,
-								 @RequestBody Usuario usuario) {
-		return this.usuarioService.editar(usuario);
+	public ResponseEntity<Usuario> editarUsuario(@PathVariable(name = "id") Long id,
+								 				 @RequestBody Usuario usuario) {
+		Optional<Usuario> usuarioDB = this.usuarioService.buscarUsurioPorId(id);
+
+		if (usuarioDB.isPresent()) {
+			return ResponseEntity.ok(this.usuarioService.editar(usuario));
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
+	@Override
 	@GetMapping
 	public Page<Usuario> listarTodos(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 			   				  		 @RequestParam(value = "size", required = false, defaultValue = "20") int size) {
 		return this.usuarioService.listarTodos(page, size);
 	}
 	
+	@Override
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> buscarEventoPorId(@PathVariable(name = "id") Long id) {
 		Optional<Usuario> usuario = this.usuarioService.buscarUsurioPorId(id);
---
+++ src/main/java/br/com/rodrigoamora/eventosti/controller/api/doc/UsuarioApiDoc.java
@@ -0,0 +1,38 @@
+package br.com.rodrigoamora.eventosti.controller.api.doc;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.rodrigoamora.eventosti.entity.Usuario;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Endpoints de usários")
+public interface UsuarioApiDoc {
+
+	@Operation(summary = "Cadastro de usário")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Cadastro de evento", content = @Content(schema = @Schema(implementation = Usuario.class))),
+	})
+	public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario);
+	
+	@Operation(summary = "Edição de usário")
+	public ResponseEntity<Usuario> editarUsuario(@PathVariable(name = "id") Long id,
+			 									 @RequestBody Usuario usuario);
+	
+	@Operation(summary = "Listagem de usários")
+	public Page<Usuario> listarTodos(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+		  		 					 @RequestParam(value = "size", required = false, defaultValue = "20") int size);
+	
+	
+	@Operation(summary = "Apagar usário pelo id")
+	public ResponseEntity<Usuario> buscarEventoPorId(@PathVariable(name = "id") Long id);
+	
+}
