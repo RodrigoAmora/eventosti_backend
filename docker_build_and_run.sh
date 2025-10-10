7bec68fc2d73434d0108ff469a263535da50942e
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-10-10 01:48:21 -0300

    Shellscript para subir os containers do Docker

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -8,6 +8,7 @@ source ~/.bash_profile
 
 ### Application ###
 rm -rf target/
+export $(grep -v '^#' .env | xargs)
 mvn clean install -Pprod -DskipTests
 
 ### Docker ###
@@ -19,21 +20,44 @@ echo -e "\n"
 
 sudo docker rmi -f rodrigoamora/rodrigo-springboot
 
-echo -e "\n\n"
-echo -e "\033[01;32m###########################\033[01;32m"
-echo -e "\033[01;32m### Building images.... ###\033[01;32m"
-echo -e "\033[01;32m###########################\033[01;32m"
-echo -e "\n\n"
+docker-compose down
+docker-compose down --rmi rodrigoamora/rodrigo-springboot
 
-sudo docker-compose build
 
-echo -e "\n\n"
-echo -e "\033[01;32m########################\033[01;32m"
-echo -e "\033[01;32m### Uping containers ###\033[01;32m"
-echo -e "\033[01;32m########################\033[01;32m"
-echo -e "\n\n"
+if [[ $(uname -s) == "Darwin" ]]; then
+  echo -e "\n"
+  echo "Running on MacOS, forcing linux/amd64"
+  echo -e "\n"
 
-sudo docker-compose up -d
+  echo -e "\n"
+  echo -e "\033[01;32m### Building images.... ###\033[01;32m"
+  echo -e "\n"
+
+  sudo docker-compose -f docker-compose-mac.yml build --no-cache
+
+  echo -e "\n"
+  echo -e "\033[01;32m### Upping containers ###\033[01;32m"
+  echo -e "\n"
+
+  sudo docker-compose -f docker-compose-mac.yml up -d --force-recreate
+
+else
+  echo -e "\n"
+  echo "Running on Linux/Windows"
+  echo -e "\n"
+
+  echo -e "\n"
+  echo -e "\033[01;32m### Building images.... ###\033[01;32m"
+  echo -e "\n"
+
+  sudo docker-compose build --no-cache
+
+  echo -e "\n"
+  echo -e "\033[01;32m### Upping containers ###\033[01;32m"
+  echo -e "\n"
+
+  sudo docker-compose up -d --force-recreate
+fi
 
 echo -e "\n"
 echo -e "\033[01;32m###############################\033[01;32m"
