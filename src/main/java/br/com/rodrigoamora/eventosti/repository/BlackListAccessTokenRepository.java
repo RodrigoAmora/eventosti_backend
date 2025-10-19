f3951ab45f1adfec8ef0989ed29355afc11cd709
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-10-19 00:12:13 -0300

    Usando a anotação Repository

--- src/main/java/br/com/rodrigoamora/eventosti/repository/BlackListAccessTokenRepository.java
+++ src/main/java/br/com/rodrigoamora/eventosti/repository/BlackListAccessTokenRepository.java
@@ -3,7 +3,9 @@ package br.com.rodrigoamora.eventosti.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.rodrigoamora.eventosti.security.token.BlackListAccessToken;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlackListAccessTokenRepository extends JpaRepository<BlackListAccessToken, String> {
 	
 	public BlackListAccessToken findByToken(String token);
